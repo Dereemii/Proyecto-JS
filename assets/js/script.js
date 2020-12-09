132689b8a5771a1a427bb772f9d7a09fcdbf096b
Dereemii <leshermira@gmail.com>
2020-12-08 22:18:30 -0300

    materialize agregado

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const obtenerPromedio = () => {
+  const nota1 = prompt("Ingresa tu calificación n°1");
+  const nota2 = prompt("Ingresa tu segunda calificación");
+
+  let promedio = (parseInt(nota1) + parseInt(nota2)) / 2;
+
+  alert(`Tu promedio es ${promedio}`);
+  console.log(`Tu promedio es ${promedio}`);
+};
