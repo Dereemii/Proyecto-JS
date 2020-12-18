b360e60e507489d8d5b1dcb14ad611ea5802c237
Dereemii <leshermira@gmail.com>
2020-12-17 21:31:56 -0300

    objeto agregado

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,30 @@ const obtenerPromedio = () => {
     }
 };
 
+/* Object constructor */
+let nombre = prompt("Hola! Bienvenid@ al sitio, por favor Ingresa tu nombre de pila");
+let apellido = prompt("Ingresa tu apellido");
+let alerta = alert("Revisa la consola")
+
+function Producto(nombre, apellido) {
+	this.nombre = nombre;
+	this.apellido = apellido;
+
+	this.mostrarNombre = function () {
+		console.log(`Hola ${this.nombre} ${this.apellido} que tengas una bonita navidad :) aquí podrás saber tu promedio de calificaciones`);
+	};
+	this.getName = function () {
+		return this.nombre;
+	};
+	this.setName = nombre;
+
+	this.getLastName = function(){
+		return this.apellido;
+	}
+	this.setLastName = apellido;
+}
+
+const producto = new Producto(nombre, apellido)
+
+producto.mostrarNombre();
+
